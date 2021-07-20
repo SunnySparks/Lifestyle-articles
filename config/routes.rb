08a78b737ccaf5142219894bf7e3dc9816adfa85
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-07-20 17:13:05 -0500

    Create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
   get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#new'
