3df3e7a9f5d8392266a25b6bbc1e9e46b376c7d6
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-07-20 17:09:43 -0500

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
