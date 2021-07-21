ccec7c55cac2219f344a61ce4ea759cd01431684
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-07-21 15:44:29 -0500

    Create articles controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/show'
-  get 'categories/update'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'sessions/create'
+
+  get 'articles/index'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'categories#index'
 
-  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  resources :users, only: [:new, :destroy, :create]
+  resources :articles, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :votes, only: [:new, :create, :destroy]
+  end
+  resources :categories, only: [:index, :new, :new, :create, :update, :show]
 
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
+
+  
 end
