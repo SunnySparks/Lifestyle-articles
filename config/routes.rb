eb3224aa4238a12900ae238519a14fcf5d7756c0
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-07-21 11:53:17 -0500

    Create categories model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/show'
+  get 'categories/update'
   get 'users/new'
   get 'users/create'
   get 'users/show'
   get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root 'categories#index'
 
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_07_21_012900) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_07_21_012900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
