e9a2dd5aff4a35cff5c364b8b97acc40587911bd
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-08-02 15:09:18 -0500

    sort articles by number of votes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   before_action :require_user, only: %i[create new]
   def index
     @article = Article.new
+    @articles = Article.all
     # @categories = Category.all.order(priority: :desc)
     @categories = Category.all.includes(:articles).order(priority: :desc)
     # @voted_article = Article.includes([:avatar_attachment]).get_most_votes
