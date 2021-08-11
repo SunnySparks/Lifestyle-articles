0685e41bd54b06ff78160000ad48d72b7094c979
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-08-10 22:00:33 -0500

    Fix Rubocop offenses

--- spec/integration/log_in_spec.rb
+++ spec/integration/log_in_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 feature 'User login' do
-
   let(:user) { User.create(name: 'User Test') }
 
   scenario 'user sees their username in the header' do
     visit login_url
-    
+
     fill_in 'name', with: user.name
     click_button 'Log In'
 
@@ -16,7 +15,7 @@ feature 'User login' do
   scenario 'user is prompted to enter a valid username if the username is empty' do
     visit login_url
     fill_in 'name', with: ''
-    
+
     click_button 'Log In'
 
     expect(current_path).to eq(root_path)
@@ -25,7 +24,7 @@ feature 'User login' do
   scenario 'user is prompted to enter a valid username if the username is wrong' do
     visit login_url
     fill_in 'name', with: 'xsadsadsa'
-    
+
     click_button 'Log In'
 
     expect(current_path).to eq(root_path)
