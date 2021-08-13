55f9c13214670dd3d464397c7d4f133d8ba154ea
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-08-12 19:35:52 -0500

    reload pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_225149) do
+ActiveRecord::Schema.define(version: 2021_08_02_224710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,7 @@ ActiveRecord::Schema.define(version: 2021_08_05_225149) do
     t.integer "cached_weighted_score", default: 0
     t.integer "cached_weighted_total", default: 0
     t.float "cached_weighted_average", default: 0.0
-    t.datetime "deleted_at"
     t.index ["category_id"], name: "index_articles_on_category_id"
-    t.index ["deleted_at"], name: "index_articles_on_deleted_at"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
