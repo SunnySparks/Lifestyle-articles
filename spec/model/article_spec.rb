c30444a8cba29b5eff118f712b25c17d20720975
José Francisco Silva Díaz <Josediazt6@outlook.com>
2021-08-16 11:04:24 -0500

    Fix rubocop offense

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Article, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
   end
-  
+
   context 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:text) }
